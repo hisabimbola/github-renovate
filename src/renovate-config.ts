c897023868dfe5c995e95fb04c86b1bc5b5524d6
Abimbola Idowu <a.idowu@sap.com>
2021-04-21 12:07:04 +0200

    chore(deps): debugging config

--- src/renovate-config.ts
+++ src/renovate-config.ts
@@ -4,7 +4,7 @@ const branchName = 'github-renovate';
 
 const config: JsonObject = {
   dryRun: false,
-  extends: ['github>hisabimbola/renovate-base:base'],
+  // extends: ['github>hisabimbola/renovate-base:base'],
   branchPrefix: `${branchName}/`,
   dependencyDashboardTitle: 'Dependency Dashboard self-hosted',
   gitAuthor: 'Renovate Bot <bot@renovateapp.com>',
